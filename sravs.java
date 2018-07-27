47277e319fd5976ffe1f6dd22a4a49f2ced8c885
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-07-27 10:34:28 +0530

    Create sravs.java

---
+++ sravs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char a= 'a';
+	if((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	System.out.println(a  +  "is an alphabet");
+	else
+	System.out.println(a + "is not an alphabet");// your code goes here
+	}
+}
